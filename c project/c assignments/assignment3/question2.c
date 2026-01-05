9dedcf485d3bd5680363acbb83e31fcd31ef371e
trishasoni1508-design <trishasoni1508@gmail.com>
2026-01-05 07:44:20 +0530

    Update question2.c

--- c project/c assignments/assignment3/question2.c
+++ c project/c assignments/assignment3/question2.c
@@ -1,95 +1,65 @@
 #include <stdio.h>
 
-void printLetter(char ch) {
-    int i, j;
-    for (i = 0; i < 5; i++) {
-        for (j = 0; j < 5; j++) {
-            if (ch == 'V') {
-                if ((j == 0 && i < 4) || (j == 4 && i < 4) || (i == 4 && j == 2) || (i == 3 && (j == 1 || j == 3)))
-                    printf("*");
-                else printf(" ");
-            }
-            else if (ch == 'A') {
-                if ((i == 0 && j > 0 && j < 4) || (i == 2) || (j == 0 && i > 0) || (j == 4 && i > 0))
-                    printf("*");
-                else printf(" ");
-            }
-            else if (ch == 'I') {
-                if (i == 0 || i == 4 || j == 2)
-                    printf("*");
-                else printf(" ");
-            }
-            else if (ch == 'B') {
-                if (j == 0 || (i == 0 && j < 4) || (i == 2 && j < 4) || (i == 4 && j < 4) || (j == 4 && i != 0 && i != 2 && i != 4))
-                    printf("*");
-                else printf(" ");
-            }
-            else if (ch == 'H') {
-                if (j == 0 || j == 4 || i == 2)
-                    printf("*");
-                else printf(" ");
-            }
-        }
-        printf("\n");
-    }
-}
-
 int main() {
-    char name[] = "VAIBHAV";
-    int k;
+    int i, j;
 
-    for (k = 0; name[k] != '\0'; k++) {
-        printf("\nPattern for %c:\n", name[k]);
-        printLetter(name[k]);
+    // Loop through rows
+    for (i = 0; i < 5; i++) {
+        // Print T
+        for (j = 0; j < 5; j++) {
+            if (i == 0 || j == 2) printf("*");
+            else printf(" ");
+        }
+        printf("  "); // space between letters
+
+        // Print R
+        for (j = 0; j < 5; j++) {
+            if (j == 0) printf("*");
+            else if ((i == 0 || i == 2) && j < 4) printf("*");
+            else if (j == 4 && i == 1) printf("*");
+            else if (i - j == 1 && i > 1) printf("*");
+            else printf(" ");
+        }
+        printf("  ");
+
+        // Print I
+        for (j = 0; j < 5; j++) {
+            if (i == 0 || i == 4 || j == 2) printf("*");
+            else printf(" ");
+        }
+        printf("  ");
+
+        // Print S
+        for (j = 0; j < 5; j++) {
+            if (i == 0 || i == 2 || i == 4) printf("*");
+            else if (i == 1 && j == 0) printf("*");
+            else if (i == 3 && j == 4) printf("*");
+            else printf(" ");
+        }
+        printf("  ");
+
+        // Print H
+        for (j = 0; j < 5; j++) {
+            if (j == 0 || j == 4 || i == 2) printf("*");
+            else printf(" ");
+        }
+        printf("  ");
+
+        // Print A
+        for (j = 0; j < 5; j++) {
+            if ((i == 0 && j > 0 && j < 4) || (i == 2) || (j == 0 && i > 0) || (j == 4 && i > 0))
+                printf("*");
+            else printf(" ");
+        }
+
+        printf("\n"); // next row
     }
 
     return 0;
-}
-//Pattern for V:
-//*   *
-/*   *
-*   *
-** **
-  *  
-
-Pattern for A:
- *** 
-*   *
-*****
-*   *
-*   *
-
-Pattern for I:
-*****
-  *  
-  *  
-  *  
-*****
-
-Pattern for B:
-****
-*   *
-****
-*   *
-**** 
-
-Pattern for H:
-*   *
-*   *
-*****
-*   *
-*   *
-
-Pattern for A:
- ***
-*   *
-*****
-*   *
-*   *
-
-Pattern for V:
-*   *
-*   *
-*   *
-** **
+}/*
+*****  ****   *****  *****  *   *   *** 
+  *    *   *    *    *      *   *  *   *
+  *    ****     *    *****  *****  *****
+  *    * *      *        *  *   *  *   *
+  *    *  *   *****  *****  *   *  *   *
   */
